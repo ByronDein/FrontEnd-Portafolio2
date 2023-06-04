3924031362d7bfc02ecdc3614cc46f8d9aef230f
Jrojal <jimmy.rojas.435@gmail.com>
2023-06-04 17:34:59 -0400

    login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,26 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'editar',
+    loadChildren: () => import('./editar/editar.module').then( m => m.EditarPageModule)
+  },
+  {
+    path: 'formulario',
+    loadChildren: () => import('./formulario/formulario.module').then( m => m.FormularioPageModule)
+  },
+  {
+    path: 'historial',
+    loadChildren: () => import('./historial/historial.module').then( m => m.HistorialPageModule)
+  },
+  {
+    path: 'registro',
+    loadChildren: () => import('./registro/registro.module').then( m => m.RegistroPageModule)
+  },
+  {
+    path: 'usuario',
+    loadChildren: () => import('./usuario/usuario.module').then( m => m.UsuarioPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,37 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  selectTabs = 'recent';
+  email: any;
+  pass: any;
+  //se llama a la libreria en el constructor
+  constructor(private router : Router, private http: HttpClient, private alertController: AlertController) {}
+async Cuentav() { 
+      const api = 'http://localhost:3000/usuarios/login';
+      const login = {
+        contrasenia: this.pass,
+        correo: this.email,
+      };
+      try {
+        const response: any = await this.http.post(api, login).toPromise();
+        console.log(response);
+        console.log(response.data);
+        console.log(response.message);
 
-  constructor() {}
-
+        if (response.data !== '' && response.message === 'Login successful') {
+          localStorage.setItem('sesion', this.email);
+          this.router.navigate(['/usuario']);
+        } 
+        else {
+          const alert = await this.alertController.create({
+            header: 'Error al iniciar sesión',
+            message: 'Ha ocurrido un error al iniciar sesión. Verifica tus credenciales e intenta de nuevo.',
+            buttons: ['OK'],
+          });
+          await alert.present();
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
 }
